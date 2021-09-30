1e03f8998379bba68f4fc9467c27f6d43d3cfb25
Mzying2001 <Mzying2001@qq.com>
2021-09-30 23:48:18 +0800

    add Init method

--- SimpleMvvm/ViewModelBase.cs
+++ SimpleMvvm/ViewModelBase.cs
@@ -5,6 +5,17 @@
     /// </summary>
     public abstract class ViewModelBase : NotificationObject
     {
-        
+        /// <summary>
+        /// Instantiate the ViewModel and call the Init method.
+        /// </summary>
+        public ViewModelBase()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// Use this method to initialize.
+        /// </summary>
+        protected virtual void Init() { }
     }
 }
