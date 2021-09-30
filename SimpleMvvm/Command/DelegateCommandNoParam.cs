d08cad68f4f59c820cd3df1daa935688fc6f49e6
Mzying2001 <Mzying2001@qq.com>
2021-09-30 22:04:10 +0800

    fix name

--- SimpleMvvm/Command/DelegateCommandNoParam.cs
+++ SimpleMvvm/Command/DelegateCommandNoParam.cs
@@ -5,24 +5,24 @@ namespace SimpleMvvm.Command
     /// <summary>
     /// Defines delegate command without parameter.
     /// </summary>
-    public class DelegeteCommand : DelegateCommand<object>
+    public class DelegateCommand : DelegateCommand<object>
     {
         /// <summary>
         /// Instantiate a DelegateCommand.
         /// </summary>
-        public DelegeteCommand() : base() { }
+        public DelegateCommand() : base() { }
 
         /// <summary>
         /// Instantiate a DelegateCommand with Execute Action.
         /// </summary>
-        public DelegeteCommand(Action executeAction) : base(o => executeAction()) { }
+        public DelegateCommand(Action executeAction) : base(o => executeAction()) { }
 
         /// <summary>
         /// Wraps the delegate as a command.
         /// </summary>
-        public static implicit operator DelegeteCommand(Action executeAction)
+        public static implicit operator DelegateCommand(Action executeAction)
         {
-            return new DelegeteCommand(executeAction);
+            return new DelegateCommand(executeAction);
         }
     }
 }
