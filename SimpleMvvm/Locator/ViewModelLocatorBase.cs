7af628fcd76d7c429ba9dff7d4f8f14d621d3564
Mzying2001 <Mzying2001@qq.com>
2021-10-11 22:14:46 +0800

    Update ViewModelLocatorBase.cs

--- SimpleMvvm/Locator/ViewModelLocatorBase.cs
+++ SimpleMvvm/Locator/ViewModelLocatorBase.cs
@@ -8,13 +8,13 @@ namespace SimpleMvvm.Locator
     /// </summary>
     public abstract class ViewModelLocatorBase
     {
-        private static readonly Dictionary<Type, ViewModelBase> _dic
+        private readonly Dictionary<Type, ViewModelBase> _dic
             = new Dictionary<Type, ViewModelBase>();
 
         /// <summary>
-        /// Register a global ViewModel instance.
+        /// Register a ViewModel instance.
         /// </summary>
-        protected static void Register<ViewModelType>() where ViewModelType : ViewModelBase, new()
+        protected void Register<ViewModelType>() where ViewModelType : ViewModelBase, new()
         {
             _dic.Add(typeof(ViewModelType), new ViewModelType());
         }
@@ -22,7 +22,7 @@ namespace SimpleMvvm.Locator
         /// <summary>
         /// Unregister the ViewModel.
         /// </summary>
-        protected static void Unregister<ViewModelType>() where ViewModelType : ViewModelBase
+        protected void Unregister<ViewModelType>() where ViewModelType : ViewModelBase
         {
             _dic.Remove(typeof(ViewModelType));
         }
@@ -30,7 +30,7 @@ namespace SimpleMvvm.Locator
         /// <summary>
         /// Get the instance of ViewModel.
         /// </summary>
-        protected static ViewModelType GetInstance<ViewModelType>() where ViewModelType : ViewModelBase
+        protected ViewModelType GetInstance<ViewModelType>() where ViewModelType : ViewModelBase
         {
             return (ViewModelType)_dic[typeof(ViewModelType)];
         }
