cc009513342e9394a286a5aa205eaa37a66abf8b
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-01-20 22:47:01 +0800

    Update ViewModelBase

--- SimpleMvvm/ViewModelBase.cs
+++ SimpleMvvm/ViewModelBase.cs
@@ -1,16 +1,41 @@
-﻿namespace SimpleMvvm
+﻿using System;
+using System.Threading;
+
+namespace SimpleMvvm
 {
     /// <summary>
     /// Base class for ViewModels.
     /// </summary>
     public abstract class ViewModelBase : NotificationObject
     {
+        private SynchronizationContext _syncContext
+            = SynchronizationContext.Current;
+
         /// <summary>
         /// Instantiate the ViewModel and call the Init method.
         /// </summary>
         public ViewModelBase()
         {
             Init();
+        }
+
+        /// <summary>
+        /// Set the UI thread context.
+        /// </summary>
+        protected void SetUIThreadContext(SynchronizationContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            _syncContext = context;
+        }
+
+        /// <summary>
+        /// Invoke an action on the UI thread.
+        /// </summary>
+        protected void InvokeOnUIThread(Action action, bool post = false)
+        {
+            if (post) _syncContext.Post(_ => action(), null);
+            else _syncContext.Send(_ => action(), null);
         }
 
         /// <summary>
