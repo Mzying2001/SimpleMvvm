b2bf14a2ee465fd0a9bbe452a1efb0606cbb18d3
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-02-24 01:28:09 +0800

    Use `WeakReference`

--- SimpleMvvm/Messaging/Messenger.cs
+++ SimpleMvvm/Messaging/Messenger.cs
@@ -8,20 +8,22 @@ namespace SimpleMvvm.Messaging
     /// </summary>
     public class Messenger
     {
-        private readonly Dictionary<string, Action<object>> _dic
-            = new Dictionary<string, Action<object>>();
-
-        private static void EmptyAction(object o) { }
+        private readonly Dictionary<string, List<WeakReference<Action<object>>>> _dic
+            = new Dictionary<string, List<WeakReference<Action<object>>>>();
 
         /// <summary>
         /// Register a delegate to receive the message.
         /// </summary>
         public void Register(string token, Action<object> action)
         {
-            if (!_dic.ContainsKey(token))
-                _dic.Add(token, EmptyAction);
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
 
-            _dic[token] += action;
+            if (!_dic.ContainsKey(token))
+                _dic.Add(token, new List<WeakReference<Action<object>>>());
+
+            var list = _dic[token];
+            list.Add(new WeakReference<Action<object>>(action));
         }
 
         /// <summary>
@@ -29,10 +31,32 @@ namespace SimpleMvvm.Messaging
         /// </summary>
         public void Unregister(string token, Action<object> action)
         {
-            _dic[token] -= action;
+            if (_dic.TryGetValue(token, out var list))
+            {
+                for (int i = list.Count - 1; i >= 0; i--)
+                {
+                    if (!list[i].TryGetTarget(out var target))
+                    {
+                        list.RemoveAt(i);
+                    }
+                    else if (target == action)
+                    {
+                        list.RemoveAt(i);
+                        break;
+                    }
+                }
 
-            if (_dic[token] == EmptyAction)
-                _dic.Remove(token);
+                if (list.Count == 0)
+                    _dic.Remove(token);
+            }
+        }
+
+        /// <summary>
+        /// Unregister all delegates.
+        /// </summary>
+        public void UnregisterAll(string token)
+        {
+            _dic.Remove(token);
         }
 
         /// <summary>
@@ -40,8 +64,16 @@ namespace SimpleMvvm.Messaging
         /// </summary>
         public void Send(string token, object message)
         {
-            if (_dic.ContainsKey(token))
-                _dic[token](message);
+            if (_dic.TryGetValue(token, out var list))
+            {
+                foreach (var weakAction in list)
+                {
+                    if (weakAction.TryGetTarget(out var action))
+                    {
+                        action(message);
+                    }
+                }
+            }
         }
 
 
