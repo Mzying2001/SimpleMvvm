d4293de448eea065d370c7fdb3a7dc5f2d049843
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-04-28 23:04:00 +0800

    Add `AsyncDelegateCommand`

---
+++ SimpleMvvm/Command/AsyncDelegateCommand.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleMvvm.Command
+{
+    /// <summary>
+    /// Defines an asynchronous delegate command.
+    /// </summary>
+    public class AsyncDelegateCommand : DelegateCommand
+    {
+        private bool _isExecuting = false;
+        private object _lock = new object();
+
+        /// <inheritdoc/>
+        public override Action<object> Execute => InvokeExecuteAsync;
+
+        /// <summary>
+        /// Asynchronous method to be called when the command is invoked.
+        /// </summary>
+        public virtual Func<object, Task> ExecuteAsync { get; set; }
+
+        /// <summary>
+        /// Invokes the ExecuteAsync method.
+        /// </summary>
+        private async void InvokeExecuteAsync(object parameter)
+        {
+            try
+            {
+                lock (_lock)
+                {
+                    if (_isExecuting)
+                        throw new InvalidOperationException("Command is executing.");
+
+                    _isExecuting = true;
+                    RaiseCanExecuteChanged();
+                }
+
+                var executeAsync = ExecuteAsync;
+                if (executeAsync != null)
+                    await executeAsync(parameter);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                lock (_lock)
+                {
+                    if (_isExecuting)
+                    {
+                        _isExecuting = false;
+                        RaiseCanExecuteChanged();
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override bool GetCanExecute()
+        {
+            return !_isExecuting && base.GetCanExecute();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand"/> class.
+        /// </summary>
+        public AsyncDelegateCommand()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand"/> class with the specified execute action.
+        /// </summary>
+        public AsyncDelegateCommand(Func<object, Task> executeAction)
+        {
+            ExecuteAsync = executeAction;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand"/> class with the specified execute action.
+        /// </summary>
+        public AsyncDelegateCommand(Func<Task> executeAction) : this(_ => executeAction())
+        {
+        }
+    }
+}
---
+++ SimpleMvvm/Command/AsyncDelegateCommandGeneric.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleMvvm.Command
+{
+    /// <summary>
+    /// Defines an asynchronous delegate command with a generic parameter.
+    /// </summary>
+    public class AsyncDelegateCommand<TParam> : AsyncDelegateCommand
+    {
+        /// <inheritdoc/>
+        public override Func<object, Task> ExecuteAsync => InvokeExecuteAsyncGeneric;
+
+        /// <summary>
+        /// Asynchronous method to be called when the command is invoked.
+        /// </summary>
+        public Func<TParam, Task> ExecuteAsyncGeneric { get; set; }
+
+        /// <summary>
+        /// Invokes the ExecuteAsyncGeneric method.
+        /// </summary>
+        private async Task InvokeExecuteAsyncGeneric(object parameter)
+        {
+            var executeAsync = ExecuteAsyncGeneric;
+            if (executeAsync != null)
+            {
+                var param = (TParam)parameter;
+                await executeAsync(param);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand{TParam}"/> class.
+        /// </summary>
+        public AsyncDelegateCommand()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommand{TParam}"/> class with the specified execute action.
+        /// </summary>
+        /// <param name="executeAsync"></param>
+        public AsyncDelegateCommand(Func<TParam, Task> executeAsync)
+        {
+            ExecuteAsyncGeneric = executeAsync;
+        }
+    }
+}
