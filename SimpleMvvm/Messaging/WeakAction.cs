7d21723302feec7ad695f63e8d0c41ecd53c5991
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-07-26 19:03:11 +0800

    Add XML documentation to WeakAction<T> class
    Added XML summary comments to all public members and methods of the WeakAction<T> class for improved code documentation and maintainability. Also updated GetHashCode and operator overloads for clarity and consistency.

--- SimpleMvvm/Messaging/WeakAction.cs
+++ SimpleMvvm/Messaging/WeakAction.cs
@@ -4,17 +4,36 @@ using System.Reflection;
 
 namespace SimpleMvvm.Messaging
 {
+    /// <summary>
+    /// Action that does not keep a strong reference to the target object.
+    /// </summary>
     internal class WeakAction<T>
     {
+        /// <summary>
+        /// The method that will be invoked when the action is called.
+        /// </summary>
         public MethodInfo Method { get; }
 
+        /// <summary>
+        /// The weak reference to the target object that contains the method.
+        /// </summary>
         public WeakReference Target { get; }
 
-        public bool IsAlive
-        {
-            get => Target.IsAlive;
-        }
+        /// <summary>
+        /// True if the target object is still alive; otherwise, false.
+        /// </summary>
+        public bool IsAlive => Target.IsAlive;
 
+        /// <summary>
+        /// Set strong reference to keep the target object alive if needed.
+        /// </summary>
+        public object KeepAliveRef { get; set; }
+
+        /// <summary>
+        /// Constructs a new instance of the <see cref="WeakAction{T}"/> class.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         public WeakAction(Action<T> action)
         {
             if (action == null)
@@ -27,11 +46,18 @@ namespace SimpleMvvm.Messaging
             Target = new WeakReference(action.Target);
         }
 
+        /// <summary>
+        /// Invokes the action with the specified argument.
+        /// </summary>
         public void Invoke(T arg)
         {
             Method.Invoke(Target.Target, new object[] { arg });
         }
 
+        /// <summary>
+        /// Tries to invoke the action with the specified argument.
+        /// </summary>
+        /// <returns>True if the action was invoked successfully; otherwise, false.</returns>
         public bool TryInvoke(T arg)
         {
             if (Method.IsStatic)
@@ -51,6 +77,7 @@ namespace SimpleMvvm.Messaging
             }
         }
 
+        /// <inheritdoc />
         public override bool Equals(object obj)
         {
             if (obj is WeakAction<T> weakAction)
@@ -83,19 +110,32 @@ namespace SimpleMvvm.Messaging
             return false;
         }
 
+        /// <inheritdoc />
         public override int GetHashCode()
         {
-            return Method.GetHashCode() ^ Target.GetHashCode();
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + (Method != null ? Method.GetHashCode() : 0);
+                hash = hash * 23 + (Target != null ? Target.GetHashCode() : 0);
+                return hash;
+            }
         }
 
+        /// <summary>
+        /// Compares two <see cref="WeakAction{T}"/> instances for equality.
+        /// </summary>
         public static bool operator ==(WeakAction<T> left, WeakAction<T> right)
         {
             return EqualityComparer<WeakAction<T>>.Default.Equals(left, right);
         }
 
+        /// <summary>
+        /// Compares two <see cref="WeakAction{T}"/> instances for inequality.
+        /// </summary>
         public static bool operator !=(WeakAction<T> left, WeakAction<T> right)
         {
-            return !(left == right);
+            return !EqualityComparer<WeakAction<T>>.Default.Equals(left, right);
         }
     }
 }
