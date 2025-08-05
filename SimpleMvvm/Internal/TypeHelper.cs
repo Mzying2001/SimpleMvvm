7faf5dbc196598531e46e3af84a61962b8eb68e2
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-08-05 20:39:27 +0800

    Add TypeHelper for safe type casting
    Introduces an internal static TypeHelper class with a generic Cast<T> method to safely cast objects and throw InvalidCastException when casting fails.

---
+++ SimpleMvvm/Internal/TypeHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SimpleMvvm.Internal
+{
+    /// <summary>
+    /// Internal helper class for type casting operations.
+    /// </summary>
+    internal static class TypeHelper
+    {
+        /// <summary>
+        /// Casts the given object to the specified type.
+        /// </summary>
+        /// <exception cref="InvalidCastException"></exception>
+        public static T Cast<T>(object obj)
+        {
+            if (obj == null)
+            {
+                return default;
+            }
+            else if (obj is T t)
+            {
+                return t;
+            }
+            else
+            {
+                throw new InvalidCastException($"Cannot cast object of type {obj.GetType()} to {typeof(T)}.");
+            }
+        }
+    }
+}
